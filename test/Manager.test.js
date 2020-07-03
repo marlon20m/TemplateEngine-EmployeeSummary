d0f6bfaee0bc1539027d22ca792d2603d890fd2b
marlon20m <marlonmora.ndr@gmail.com>
2020-07-02 22:35:19 -0500

    adding team.html generator

--- test/Manager.test.js
+++ test/Manager.test.js
@@ -1,11 +1,12 @@
 const Manager = require("../lib/Manager");
 const Employee = require("../lib/Employee");
 
-// test("Can set office number via constructor argument", () => {
-//   const testValue = 100;
-//   const e = new Manager("Foo", 1, "test@test.com", testValue);
-//   expect(e.officeNumber).toBe(testValue);
-// });
+test("Set office number with the constructor argument", () => {
+  const testValue = 100;
+  const e = new Manager("Foo", 1, "test@test.com", testValue);
+  expect(e.officeNumber).toBe(testValue);
+});
+
 
 test('getRole() should return "Manager"', () => {
   const testValue = "Manager";
@@ -13,8 +14,8 @@ test('getRole() should return "Manager"', () => {
   expect(e.getRole()).toBe(testValue);
 });
 
-// test("Can get office number via getOffice()", () => {
-//   const testValue = 100;
-//   const e = new Manager("Foo", 1, "test@test.com", testValue);
-//   expect(e.getOfficeNumber()).toBe(testValue);
-// });
+test("Can get office number via getOffice()", () => {
+  const testValue = 100;
+  const e = new Manager("Foo", 1, "test@test.com", testValue);
+  expect(e.getOfficeNumber()).toBe(testValue);
+});
